8de6c39d2e8a90a48f7cf16e656b3ee00c5ca7ee
Max Unsted <maxunsted@gmail.com>
2023-01-21 18:01:42 +0000

    backup

--- firmware/keezus/src/layout.rs
+++ firmware/keezus/src/layout.rs
@@ -29,16 +29,16 @@ pub static LAYERS: keyberon::layout::Layers<NUM_COLS, NUM_ROWS, NUM_LAYERS, Cust
 */
 [
 // left side                                                                                                                                              
-[k(Escape),       k(Grave),      k(Kb1),   k(Kb2),  k(Kb3),   k(Kb4),   k(Kb5),   k(F1),    k(F2),     k(F3),       Trans,        Trans,       Trans,     Trans,   Trans,     Trans],
-[Trans,           k(Tab),        k(Q),     k(W),    k(E),     k(R),     k(T),     k(F4),    k(F5),     k(F6),       Trans,        Trans,       Trans,     Trans,   Trans,     Trans],
-[Trans,           k(BSpace),     k(A),     k(S),    k(D),     k(F),     k(G),     k(F7),    k(F8),     k(F9),       Trans,        Trans,       Trans,     Trans,   Trans,     Trans],
-[k(NonUsBslash),  k(LShift),     k(Z),     k(X),    k(C),     k(V),     k(B),     k(F10),   k(F11),    k(F12),      Trans,        Trans,       Trans,     Trans,   Trans,     Trans],
-[Trans,           k(LCtrl),      k(LGui),  Trans,   k(LAlt),  k(LCtrl), k(Space), Trans,    Trans,     Trans,       Trans,        Trans,       Trans,     Trans,   Trans,     Trans,],
+[k(Escape),       Trans,         k(Home),  Trans,   k(End),   Trans,    k(End),   Trans,    Trans,       Trans,    Trans,       Trans,        Trans,     Trans,   Trans,     Trans],
+[k(RAlt),         Trans,         k(Grave), k(W),    k(E),     k(R),     k(T),     Trans,    Trans,       k(F1),    k(F2),       k(F3),        Trans,     Trans,   Trans,     Trans],
+[k(Tab),          Trans,         k(Q),     k(S),    k(D),     k(F),     k(G),     Trans,    Trans,       k(F4),    k(F5),       k(F6),        Trans,     Trans,   Trans,     Trans],
+[k(BSpace),       Trans,         k(A),     k(X),    k(C),     k(V),     k(B),     Trans,    Trans,       k(F7),    k(F8),       k(F9),        Trans,     Trans,   Trans,     Trans],
+[k(LShift),       Trans,         k(Z),     k(LAlt), k(LGui),  k(LCtrl), k(Space), Trans,    Trans,       k(F10),   k(F11),      k(F12),       Trans,     Trans,   Trans,     Trans,],
 // right side                                                                                                                                                   
-[k(MediaVolDown), k(MediaVolUp), k(Mute),  Trans,   k(Kb6),   k(Kb7),   k(Kb8),   k(Kb9),   k(Kb0),    k(Minus),    k(Equal),     k(CapsLock), k(Insert), Trans,   Trans,     Trans],
-[k(Kp1),          k(Kp2),        k(Kp3),   Trans,   k(Y),     k(U),     k(I),     k(O),     k(P),      k(LBracket), k(RBracket),  k(Enter),    k(Delete), k(Home), k(End),    Trans],
-[k(Kp4),          k(Kp5),        k(Kp6),   Trans,   k(H),     k(J),     k(K),     k(L),     k(SColon), k(Quote),    k(NonUsHash), k(Enter),    k(PgUp),   k(Up),   k(PgDown), Trans],
-[k(Kp7),          k(Kp8),        k(Kp9),   k(Kp0),  k(N),     k(M),     k(Comma), k(Dot),   k(Slash),  k(RShift),   k(RShift),    Trans,       k(Left),   k(Down), k(Right),  Trans],
-[Trans,           Trans,         Trans,    Trans,   k(Space), k(RCtrl), Trans,    Trans,    Trans,     k(Space),    Trans,        k(RCtrl),    Trans,     Trans,   Trans,     Trans,],
+[k(MediaVolDown), k(MediaVolUp), k(Mute),  Trans,   k(Left),  Trans,    k(Right), Trans,    k(PgUp),     Trans,    k(PgDown),    k(CapsLock), k(Insert), Trans,   Trans,     Trans],
+[k(Kb1),          k(Kb2),        k(Kb3),   Trans,   k(Y),     k(U),     k(I),     k(O),     k(LBracket), Trans,    k(RBracket),  k(Enter),    k(Delete), k(Home), k(End),    Trans],
+[k(Kb4),          k(Kb5),        k(Kb6),   k(Up),   k(H),     k(J),     k(K),     k(L),     k(P),        Trans,    k(Quote),     k(Enter),    k(PgUp),   k(Up),   k(PgDown), Trans],
+[k(Kb7),          k(Kb8),        k(Kb9),   Trans,   k(N),     k(M),     k(Comma), k(Dot),   k(SColon),   Trans,    k(Enter),     Trans,       k(Left),   k(Down), k(Right),  Trans],
+[k(Minus),        k(Kb0),        k(Equal), k(Down), k(Space), k(RCtrl), k(Left),  k(Right),    k(Slash),    Trans,    k(RShift),    k(RCtrl),    Trans,     Trans,   Trans,     Trans,],
 ] 
 ];
